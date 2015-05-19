FakeSmile/FakeSmile

commit b95657d9c5a74765997d4d89f14b8a85f0a7ba87

Minor file size decrease
General:
Rework property defaults JSON-like object to a JavaScript native object
(for consistency with the named colors object);
(slight file size decrease).

smil.user.js
@@ -1360,66 +1360,66 @@ var colors = {
 };
 
 var propDefaults = {
-	"font" : "see individual properties",
-	"font-family" : "Arial",
-	"font-size" : "medium",
-	"font-size-adjust" : "none",
-	"font-stretch" : "normal",
-	"font-style" : "normal",
-	"font-variant" : "normal",
-	"font-weight" : "normal",
-	"direction" : "ltr",
-	"letter-spacing" : "normal",
-	"text-decoration" : "none",
-	"unicode-bidi" : "normal",
-	"word-spacing" : "normal",
-	"clip" : "auto",
-	"color" : "depends on user agent",
-	"cursor" : "auto",
-	"display" : "inline",
-	"overflow" : "hidden",
-	"visibility" : "visible",
-	"clip-path" : "none",
-	"clip-rule" : "nonzero",
-	"mask" : "none",
-	"opacity" : "1",
-	"enable-background" : "accumulate",
-	"filter" : "none",
-	"flood-color" : "black",
-	"flood-opacity" : "1",
-	"lighting-color" : "white",
-	"stop-color" : "black",
-	"stop-opacity" : "1",
-	"pointer-events" : "visiblePainted",
-	"color-interpolation" : "sRGB",
-	"color-interpolation-filters" : "linearRGB",
-	"color-profile" : "auto",
-	"color-rendering" : "auto",
-	"fill" : "black",
-	"fill-opacity" : "1",
-	"fill-rule" : "nonzero",
-	"image-rendering" : "auto",
-	"marker-end" : "none",
-	"marker-mid" : "none",
-	"marker-start" : "none",
-	"shape-rendering" : "auto",
-	"stroke" : "none",
-	"stroke-dasharray" : "none",
-	"stroke-dashoffset" : "0",
-	"stroke-linecap" : "butt",
-	"stroke-linejoin" : "miter",
-	"stroke-miterlimit" : "4",
-	"stroke-opacity" : "1",
-	"stroke-width" : "1",
-	"text-rendering" : "auto",
-	"alignment-baseline" : "0",
-	"baseline-shift" : "baseline",
-	"dominant-baseline" : "auto",
-	"glyph-orientation-horizontal" : "0",
-	"glyph-orientation-vertical" : "auto",
-	"kerning" : "auto",
-	"text-anchor" : "start",
-	"writing-mode" : "lr-tb"
+	font : "see individual properties",
+	font-family : "Arial",
+	font-size : "medium",
+	font-size-adjust : "none",
+	font-stretch : "normal",
+	font-style : "normal",
+	font-variant : "normal",
+	font-weight : "normal",
+	direction : "ltr",
+	letter-spacing : "normal",
+	text-decoration : "none",
+	unicode-bidi : "normal",
+	word-spacing : "normal",
+	clip : "auto",
+	color : "depends on user agent",
+	cursor : "auto",
+	display : "inline",
+	overflow : "hidden",
+	visibility : "visible",
+	clip-path : "none",
+	clip-rule : "nonzero",
+	mask : "none",
+	opacity: 1,
+	enable-background : "accumulate",
+	filter : "none",
+	flood-color : "black",
+	flood-opacity: 1,
+	lighting-color : "white",
+	stop-color : "black",
+	stop-opacity: 1,
+	pointer-events : "visiblePainted",
+	color-interpolation : "sRGB",
+	color-interpolation-filters : "linearRGB",
+	color-profile : "auto",
+	color-rendering : "auto",
+	fill : "black",
+	fill-opacity: 1,
+	fill-rule : "nonzero",
+	image-rendering : "auto",
+	marker-end : "none",
+	marker-mid : "none",
+	marker-start : "none",
+	shape-rendering : "auto",
+	stroke : "none",
+	stroke-dasharray : "none",
+	stroke-dashoffset: 0,
+	stroke-linecap : "butt",
+	stroke-linejoin : "miter",
+	stroke-miterlimit: 4,
+	stroke-opacity: 1,
+	stroke-width: 1,
+	text-rendering : "auto",
+	alignment-baseline: 0,
+	baseline-shift : "baseline",
+	dominant-baseline : "auto",
+	glyph-orientation-horizontal: 0,
+	glyph-orientation-vertical : "auto",
+	kerning : "auto",
+	text-anchor : "start",
+	writing-mode : "lr-tb"
 };
 
 function funk(func, obj, arg) {

commit f3cb93bc0efb2b18a343216c409fb0c220e12660

Fix regression regarding variable names
Bug fix:
Variables cannot contain dashes, as they are seen as a subtraction
expression
(therefore, in those cases, enclosing in quotes is required);
(caught by Google Closure - thanks!);
(minor regression introduced in
b95657d9c5a74765997d4d89f14b8a85f0a7ba87).

smil.user.js
@@ -1362,67 +1362,69 @@ var colors = {
 	yellowgreen : [154, 205, 50]
 };
 
+// NOTE: variables cannot contain dashes, as they are seen as a subtraction expression
+// (therefore, in those cases, enclosing in quotes is required)
 var propDefaults = {
 	font : "see individual properties",
-	font-family : "Arial",
-	font-size : "medium",
-	font-size-adjust : "none",
-	font-stretch : "normal",
-	font-style : "normal",
-	font-variant : "normal",
-	font-weight : "normal",
+	"font-family" : "Arial",
+	"font-size" : "medium",
+	"font-size-adjust" : "none",
+	"font-stretch" : "normal",
+	"font-style" : "normal",
+	"font-variant" : "normal",
+	"font-weight" : "normal",
 	direction : "ltr",
-	letter-spacing : "normal",
-	text-decoration : "none",
-	unicode-bidi : "normal",
-	word-spacing : "normal",
+	"letter-spacing" : "normal",
+	"text-decoration" : "none",
+	"unicode-bidi" : "normal",
+	"word-spacing" : "normal",
 	clip : "auto",
 	color : "depends on user agent",
 	cursor : "auto",
 	display : "inline",
 	overflow : "hidden",
 	visibility : "visible",
-	clip-path : "none",
-	clip-rule : "nonzero",
+	"clip-path" : "none",
+	"clip-rule" : "nonzero",
 	mask : "none",
 	opacity: 1,
-	enable-background : "accumulate",
+	"enable-background" : "accumulate",
 	filter : "none",
-	flood-color : "black",
-	flood-opacity: 1,
-	lighting-color : "white",
-	stop-color : "black",
-	stop-opacity: 1,
-	pointer-events : "visiblePainted",
-	color-interpolation : "sRGB",
-	color-interpolation-filters : "linearRGB",
-	color-profile : "auto",
-	color-rendering : "auto",
+	"flood-color" : "black",
+	"flood-opacity" : 1,
+	"lighting-color" : "white",
+	"stop-color" : "black",
+	"stop-opacity" : 1,
+	"pointer-events" : "visiblePainted",
+	"color-interpolation" : "sRGB",
+	"color-interpolation-filters" : "linearRGB",
+	"color-profile" : "auto",
+	"color-rendering" : "auto",
 	fill : "black",
-	fill-opacity: 1,
-	fill-rule : "nonzero",
-	image-rendering : "auto",
-	marker-end : "none",
-	marker-mid : "none",
-	marker-start : "none",
-	shape-rendering : "auto",
+	"fill-opacity" : 1,
+	"fill-rule" : "nonzero",
+	"image-rendering" : "auto",
+	"marker-end" : "none",
+	"marker-mid" : "none",
+	"marker-start" : "none",
+	"shape-rendering" : "auto",
 	stroke : "none",
-	stroke-dasharray : "none",
-	stroke-dashoffset: 0,
-	stroke-linecap : "butt",
-	stroke-linejoin : "miter",
-	stroke-miterlimit: 4,
-	stroke-opacity: 1,
-	stroke-width: 1,
-	text-rendering : "auto",
-	alignment-baseline: 0,
-	baseline-shift : "baseline",
-	dominant-baseline : "auto",
-	glyph-orientation-horizontal: 0,
-	glyph-orientation-vertical : "auto",
+	"stroke-dasharray" : "none",
+	"stroke-dashoffset" : 0,
+	"stroke-linecap" : "butt",
+	"stroke-linejoin" : "miter",
+	"stroke-miterlimit" : 4,
+	"stroke-opacity" : 1,
+	"stroke-width" : 1,
+	"text-rendering" : "auto",
+	"alignment-baseline" : 0,
+	"baseline-shift" : "baseline",
+	"dominant-baseline" : "auto",
+	"glyph-orientation-horizontal" : 0,
+	"glyph-orientation-vertical" : "auto",
 	kerning : "auto",
-	text-anchor : "start",
-	writing-mode : "lr-tb"
+	"text-anchor" : "start",
+	"writing-mode" : "lr-tb"
 };
 
 function funk(func, obj, arg) {