FakeSmile/FakeSmile

commit 9f26b2c3db7b50834a281f1b35a48e530ab64ecf

Bug fix:   Console object was being used regardless of availability     (it's only available in IE when Developer Tools are open);     (test case available at http://leunen.me/fakesmile/example2.svg).

smil.user.js
@@ -1040,9 +1040,13 @@ function animate() {
       if (!animations[i].f(curTime))
         i--;
     } catch(exc) {
-      if (exc.message!="Component returned failure code: 0x80004005 (NS_ERROR_FAILURE) [nsIDOMSVGPathElement.getTotalLength]") {
+      if (exc.message!=="Component returned failure code: 0x80004005 (NS_ERROR_FAILURE) [nsIDOMSVGPathElement.getTotalLength]") {
+        // NOTE: in IE, console object is only available when Developer tools are open
+        if(window.console && console.log) {
         console.log(exc);
-        //alert(exc);
+        } else {
+          alert(exc);
+        }
       }
     }
   }

commit 68b82a7ab811f7ca158b2f681f811765e0cbb185

Disable alert-based error display
Bug fix:
  Internal errors were user-displayed regardless of being
recovered afterwards
    (intrusive alert-based errors are now commented
out by default);
    (regression introduced in changeset 9f26b2c).

smil.user.js
@@ -1109,8 +1109,9 @@ function animate() {
 				// NOTE: in IE, console object is only available when Developer tools are open
 				if (window.console && console.log) {
 					console.log(exc);
-				} else {
-					alert(exc);
+				// uncomment to force error display
+				//} else {
+				//	alert(exc);
 				}
 			}
 		}